eb6d0f07b5319b2f630c020709a05d62b129e088
iunmei <61954806+iunmei@users.noreply.github.com>
2025-12-07 13:51:08 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -9,12 +9,12 @@ const CONFIG = {
 
   NEXT_NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
-  NEXT_POST_LIST_COVER: false, // 文章列表显示封面图
+  NEXT_POST_LIST_COVER: true, // 文章列表显示封面图
   NEXT_POST_LIST_PREVIEW: true, // 显示文章预览
   NEXT_POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
 
-  NEXT_PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
-  NEXT_PREVIEW_CATEGORY_COUNT: 16, // 首页最多展示的分类数量，0为不限制
+  NEXT_PREVIEW_TAG_COUNT: 7, // 首页最多展示的标签数量，0为不限制
+  NEXT_PREVIEW_CATEGORY_COUNT: 0, // 首页最多展示的分类数量，0为不限制
   NEXT_POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
   // 右侧组件
@@ -22,7 +22,7 @@ const CONFIG = {
   NEXT_RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
   NEXT_RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   NEXT_RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
-  NEXT_RIGHT_AD: false, // 右侧广告
+  NEXT_RIGHT_AD: true, // 右侧广告
 
   // 菜单
   NEXT_MENU_HOME: true, // 显示首页
@@ -32,8 +32,8 @@ const CONFIG = {
   NEXT_MENU_SEARCH: true, // 显示搜索
 
   NEXT_WIDGET_TO_TOP: true, // 是否显示回顶
-  NEXT_WIDGET_TO_BOTTOM: false, // 显示回底
-  NEXT_WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
+  NEXT_WIDGET_TO_BOTTOM: true, // 显示回底
+  NEXT_WIDGET_DARK_MODE: true, // 显示日间/夜间模式切换
   NEXT_WIDGET_TOC: true, // 移动端显示悬浮目录
 
   NEXT_ARTICLE_RELATE_POSTS: true, // 相关文章推荐
