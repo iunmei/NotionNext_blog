d8696c6d5d352c2ce663c27e04c29ca5ef17d066
iunmei <61954806+iunmei@users.noreply.github.com>
2025-12-07 13:55:37 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   NEXT_POST_LIST_PREVIEW: true, // 显示文章预览
   NEXT_POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
 
-  NEXT_PREVIEW_TAG_COUNT: 7, // 首页最多展示的标签数量，0为不限制
+  NEXT_PREVIEW_TAG_COUNT: 0, // 首页最多展示的标签数量，0为不限制
   NEXT_PREVIEW_CATEGORY_COUNT: 0, // 首页最多展示的分类数量，0为不限制
   NEXT_POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
